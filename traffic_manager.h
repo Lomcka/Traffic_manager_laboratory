8d6a51ff31edab72b842739b9b713bc13d5d555c
Dmitriy <Jlomckaaa@yandex.ru>
2022-03-02 14:22:11 +0300

    Final
    Testes and fixed

---
+++ traffic_manager.h
@@ -0,0 +1,43 @@
+#ifndef TRAFFIC_MANAGER_H_
+#define TRAFFIC_MANAGER_H_
+
+#include <vector>
+#include "graph.h"
+
+class TrafficManager {
+ public:
+  TrafficManager(const Graph& graph,
+                  const std::vector<int>& buns_amounts,
+                  const std::vector<int>& vehicles,
+                  int vehicle_capacity) : country_(graph),
+                                          buns_(buns_amounts),
+                                          vehicles_(vehicles),
+                                          vehicle_capacity_(vehicle_capacity) {}
+
+  int GetTotalBunsAmount();
+  int GetTotalVehiclesAmount();
+  int GetBunsAmountInCity(int city);
+  int GetVehiclesAmountInCity(int city);
+
+  std::vector<int> GetBunsAmounts();
+  std::vector<int> GetVehiclesAmounts();
+
+  void SetBunsAmounts(const std::vector<int>& buns);
+  void SetBunsAmountInCity(int city, int buns);
+  void SetVehiclesAmounts(const std::vector<int>& vehicles);
+  void SetVehiclesAmountInCity(int city, int vehicles);
+
+  int MoveVehicles(int from, int to, int count);
+  int Transport(int from, int to, int buns_amount);
+
+ private:
+  int GetShortestPathLength(int from, int to);
+  void TransferVehicles(int from, int to, int count);
+
+  Graph country_;
+  std::vector<int> buns_;
+  std::vector<int> vehicles_;
+  int vehicle_capacity_;
+};
+
+#endif  // TRAFFIC_MANAGER_H_
---
+++ traffic_manager_tests.cpp
@@ -0,0 +1,41 @@
+#include "traffic_manager.h"
+#include <gtest/gtest.h>
+
+TEST(Manager, GettersAndSetters) {
+  Graph a(5);
+  std::vector<int> buns = {1, 2, 3, 4, 5};
+  std::vector<int> vehicles = {1, 1, 1, 1, 1};
+  TrafficManager m(a, buns, vehicles, 1);
+  ASSERT_EQ(m.GetTotalBunsAmount(), 15);
+  m.SetBunsAmountInCity(0, 6);
+  ASSERT_EQ(m.GetTotalBunsAmount(), 20);
+  ASSERT_EQ(m.GetVehiclesAmountInCity(2), 1);
+  buns[0] = 6;
+  ASSERT_EQ(m.GetBunsAmounts(), buns);
+}
+
+TEST(Manager, Transport) {
+  Graph a(5);
+  std::vector<int> buns = {1, 2, 3, 4, 5};
+  std::vector<int> vehicles = {1, 1, 1, 1, 1};
+  TrafficManager m(a, buns, vehicles, 1);
+  ASSERT_EQ(m.MoveVehicles(0, 2, 1), 1);
+  ASSERT_EQ(m.GetVehiclesAmountInCity(2), 2);
+  ASSERT_EQ(m.Transport(4, 0, 5), 2);
+  ASSERT_EQ(m.GetBunsAmountInCity(0), 6);
+}
+
+TEST(Manager, Tricky) {
+  std::vector<std::vector<Graph::Edge>> edges = {
+      {Graph::Edge(1, 5)},
+      {Graph::Edge(0, 5), Graph::Edge(2, 3)},
+      {Graph::Edge(1, 3), Graph::Edge(3, 10)},
+      {Graph::Edge(2, 10)}
+  };
+
+  std::vector<int> buns = {1, 2, 30, 4};
+  std::vector<int> vehicles = {1, 1, 1, 1};
+  Graph a(edges);
+  TrafficManager m(a, buns, vehicles, 1);
+  ASSERT_EQ(m.Transport(2, 3, 3), 18);
+}
